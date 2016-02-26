97fb7448ad08667df45991ee7906f2097ef2b26a
hannhuan <hannhuan@hotmail.com>
2016-02-26 11:48:44 +0100

    Added files via upload

---
+++ EDA216pro.sql
@@ -0,0 +1,7 @@
+set foreign_key_checks = 0;
+drop table if exists theatre;
+drop table if exists movies;
+drop table if exists timeSlot;
+drop table if exists Reservation;
+drop table if exists user;
+set foreign_key_checks = 1;
