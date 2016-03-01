661f0711454ed6459cf3f565668e142c418c3cdd
HannaLindwall <HannaLindwall@users.noreply.github.com>
2016-03-01 13:24:30 +0100

    Update EDA216pro.sql

--- EDA216pro.sql
+++ EDA216pro.sql
@@ -28,9 +28,19 @@ create table customers (
 
 create table orders(
 	customerName varchar (255) not null,
+	cookieName	varchar(255) not null,
+	nbrPallets	int (20) default '0',
 	deliveryDate date,
 	ifDelivered char (1) default 'n',
-	foreign key (customerName) references customers (customerName)
+	primary key(customerName, cookieName, deliveryDate),
+	foreign key (customerName) references customers (customerName) on update cascade,
+	foreign key (cookieName) references cookies (cookieName) on update cascade
+);
+// Tänkte att detta blir bättre
+create table recipes(
+	cookieName varchar (255) not null,
+	ingredientInfo varchar(500) not null,
+	foreignkey(cookieName) references cookies (cookieName) on update cascade
 );
 
 create table recipes(
@@ -83,6 +93,15 @@ insert into customers value ('Kalaskakor AB', 'Trelleborg');
 insert into customers value ('Partykakor AB', 'Kristianstad');
 insert into customers value ('Gästkakor AB', 'Hässleholm');
 insert into customers value ('Skånekakor AB', 'Perstorp');
+
+insert into recipes values
+('Nut Ring', 'flour 450 g; butter 450 g; icing Sugar 190 g; roasted, Chopped Nuts 225 g'),
+('Nut Cookie', 'fine-Ground Nuts 750 g; ground, Roasted Nuts 625 g; bread Crumbs 125 g; sugar 375 g; egg Whites 3.5 dl; chocolate 50 g'),
+('Amneris', 'Marzipan 750 g; Butter 250 g; Eggs 250 g; Potato starch 25 g; Wheat flour 25 g'),
+('Tango', 'Butter 200 g; Sugar 250 g; Flour 300 g; Sodium bicarbonate 4 g; Vanilla 2 g'),
+('Almond Delight', 'Butter 400 g; Sugar 270 g; Chopped almonds 279 g; Flour 400 g; Cinnamon 10 g'),
+('Berliner', 'Flour 350 g; Butter 250 g; Icing sugar 100 g; Eggs 50 g; Vanilla sugar 5 g; Chocolate 50 g');
+
 
 insert into recipes(cookieName, flour, butter, icingSugar, roastedChoppedNuts) 
 value ('Nut Ring', 450, 450, 190, 225);
